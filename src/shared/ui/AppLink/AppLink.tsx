8297136ba205622c874b18774e14554db3c9509f
Vladislav Milko <vladmilko27@gmail.com>
2022-10-13 23:53:50 +0300

    Вынос навбара и ссылок

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './AppLink.module.scss';
+
+export enum AppLinkTheme {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = ({
+  className,
+  theme = AppLinkTheme.PRIMARY,
+  children,
+  ...otherProps
+}) => {
+  return (
+    <Link
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import cls from './Navbar.module.scss';
+
+type NavbarProps = {
+  classname?: string;
+};
+
+export const Navbar = ({ classname }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.Navbar, {}, [classname])}>
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={'/'}
+          className={cls.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
