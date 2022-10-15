e88c16ebebd080849cdf8c2a4c190be13759c02c
Vladislav Milko <vladmilko27@gmail.com>
2022-10-15 21:39:18 +0300

    Обновление pages и index

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <AppRouter />
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
