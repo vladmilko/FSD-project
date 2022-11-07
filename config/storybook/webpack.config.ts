69cc25cd1c0f37592f1cbf9847beeadb316d6a78
Vladislav Milko <vladmilko27@gmail.com>
2022-11-08 00:31:16 +0300

    Корректировка типизации webpack.config для сторибука

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,12 +11,11 @@ export default ({ config }: { config: webpack.Configuration }) => {
     src: path.resolve(__dirname, '..', '..', 'src'),
   };
 
-  config.resolve?.modules?.push(paths.src);
-  config.resolve?.extensions?.push('.ts', '.tsx');
+  config.resolve!.modules!.push(paths.src);
+  config.resolve!.extensions!.push('.ts', '.tsx');
 
   // @ts-ignore
-  // eslint-disable-next-line no-param-reassign
-  config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
+  config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
@@ -24,13 +23,13 @@ export default ({ config }: { config: webpack.Configuration }) => {
     return rule;
   });
 
-  config.module?.rules?.push({
+  config.module!.rules!.push({
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   });
   config.module?.rules?.push(buildCssLoader(true));
 
-  config.plugins?.push(
+  config.plugins!.push(
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(true),
       __API__: JSON.stringify(''),
