5dabad61e0f5e0a0d0e18070c423b686c7ac758a
Vladislav Milko <vladmilko27@gmail.com>
2022-12-08 23:05:14 +0300

    Добавление компонента Dropdown

--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -37,7 +37,7 @@ export const CurrencySelect = memo(
         items={options}
         onChange={onChangeHandler}
         readonly={readonly}
-        direction="top"
+        direction="topRight"
         className={className}
       />
     );
--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -9,6 +9,13 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: 100 }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => (
@@ -16,4 +23,40 @@ const Template: ComponentStory<typeof ListBox> = (args) => (
 );
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+  value: '123',
+  items: [
+    { content: '1asfasfasf23', value: '123' },
+    { content: '1asfasfasf21233', value: '1232' },
+  ],
+};
+
+export const BottomLeft = Template.bind({});
+BottomLeft.args = {
+  direction: 'bottomLeft',
+  value: '123',
+  items: [
+    { content: '1asfasfasf23', value: '123' },
+    { content: '1asfasfasf21233', value: '1232' },
+  ],
+};
+
+export const TopLeft = Template.bind({});
+TopLeft.args = {
+  direction: 'topLeft',
+  value: '123',
+  items: [
+    { content: '1asfasfasf23', value: '123' },
+    { content: '1asfasfasf21233', value: '1232' },
+  ],
+};
+
+export const TopRight = Template.bind({});
+TopRight.args = {
+  direction: 'topRight',
+  value: '123',
+  items: [
+    { content: '1asfasfasf23', value: '123' },
+    { content: '1asfasfasf21233', value: '1232' },
+  ],
+};
