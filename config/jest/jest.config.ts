1a9aa7225cd731063cd0eb98806695fb2513a792
vladmilko <vladmilko27@gmail.com>
2023-01-17 00:24:11 +0300

    update screen tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -41,12 +41,7 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
-    '^@/shared(.*)$': '<rootDir>/src/shared$1',
-    '^@/entities(.*)$': '<rootDir>/src/entities$1',
-    '^@/features(.*)$': '<rootDir>/src/features$1',
-    '^@/widgets(.*)$': '<rootDir>/src/widgets$1',
-    '^@/pages(.*)$': '<rootDir>/src/pages$1',
-    '^@/app(.*)$': '<rootDir>/src/app$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 
   // All imported modules in your tests should be mocked automatically
