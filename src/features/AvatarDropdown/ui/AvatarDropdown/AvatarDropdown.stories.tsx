0997a6fabe272bef2b2f141f8240db78c8deb9df
vladmilko <vladmilko27@gmail.com>
2023-01-21 19:20:54 +0300

    Correct ci settings and ui sreens

--- src/features/AvatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
+++ src/features/AvatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { AvatarDropdown } from './AvatarDropdown';
 
 export default {
@@ -9,11 +9,13 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [StoreDecorator({ user: { authData: {} } })],
 } as ComponentMeta<typeof AvatarDropdown>;
 
 const Template: ComponentStory<typeof AvatarDropdown> = (args) => (
   <AvatarDropdown {...args} />
 );
 
+// TODO: Fix dropdown position problem
 export const Normal = Template.bind({});
 Normal.args = {};
